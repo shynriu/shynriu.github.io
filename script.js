26cbbc61dbc1e52e32c0ac6b368045be09cbf28e
Jimmy Tran <jimmy.tran@epitech.eu>
2023-09-10 02:33:51 +0200

    v1

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // script.js
 const backgroundElement = document.querySelector('.background');
+const circleElement = document.querySelector('.circle');
+const logoElement = document.querySelector('.logo');
 
 anime({
     targets: backgroundElement,
@@ -8,3 +10,21 @@ anime({
     loop: true, // Loop the animation
     easing: 'linear' // Linear easing for continuous movement
 });
+
+
+
+/*anime({
+    targets: circleElement,
+    keyframes: [
+        {translateY: -40},
+        {translateX: 250},
+        {translateY: 40},
+        {translateX: 0},
+        {translateY: 0}
+    ],
+    duration: 4000, // Animation duration in milliseconds
+    loop: true, // Loop the animation
+    easing: 'easeOutElastic(1, .8)',
+});*/
+
+
